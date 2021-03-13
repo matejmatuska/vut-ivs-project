b89845cf53a18eba04015602860d67f61fee6b10
Martin Pokorný <xpokor85@stud.fit.vutbr.cz>
2021-03-14 00:22:05 +0100

    Repair Commit II. -Class rename -Solution relocation

---
+++ src/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IVS_GUI
+{
+    public class Controller : GuiKalk
+    {
+        /// <summary>
+        /// Hlavní vstupní bod aplikace.
+        /// </summary>
+        /// 
+        GuiKalk gui = new GuiKalk();
+        [STAThread]
+        
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GuiKalk());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            gui.Outcome.Text = "Text";
+
+        }
+
+    }
+}
