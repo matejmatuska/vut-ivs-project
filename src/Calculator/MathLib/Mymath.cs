511fc7a9a792f8835b16a1d491390205b8a48f55
Júlia Mazáková <mazakovajulia@gmail.com>
2021-03-15 20:56:08 +0100

    fix MathTests

---
+++ src/Calculator/MathLib/Mymath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyMath
+{
+
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+           throw new NotImplementedException();
+        }
+    }
+
+    public class Mymathclass
+    {
+        public double Sum(double input1, double input2)
+        {
+            throw new NotImplementedException();
+           
+          
+        }
+
+        public double Sub(double input1, double input2)
+        {
+            throw new NotImplementedException();
+           
+        }
+
+        public double Mul(double input1, double input2)
+        {
+            throw new NotImplementedException();
+           
+        }
+
+        public double Div(double input1, double input2)
+        {
+            throw new NotImplementedException();
+           
+        }
+    }
+}
