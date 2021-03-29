eb836fc794180f08b1842ef9e7622df85fce92a6
schaferm72 <lukral72@seznam.cz>
2021-03-29 21:26:23 +0200

    change of the variable names x1, x2, x(base), n(root)

--- src/Calculator/MathLib/Mymath.cs
+++ src/Calculator/MathLib/Mymath.cs
@@ -9,79 +9,79 @@ namespace MyMathLib
         /*
          * sums 2 double numbers together
          *
-         *@param    input1,input2     numbers that you want to sum
+         *@param    x1, x2     numbers that you want to sum
          *@return   the result of the sum in double
          */
-        public static double Sum(double input1, double input2)
+        public static double Sum(double x1, double x2)
         {
-            return input1 + input2;
+            return x1 + x2;
         }
 
         /*
          * subtracts 2 double numbers
          *
-         *@param    input1,input2     numbers that you want to subtract
+         *@param    x1, x2     numbers that you want to subtract
          *@return   the result of the subtraction in double
          */
-        public static double Sub(double input1, double input2)
+        public static double Sub(double x1, double x2)
         {
-            return input1 - input2;
+            return x1 - x2;
         }
 
         /*
          * multiplies 2 double numbers
          *
-         *@param    input1,input2     numbers that you want to multiply
+         *@param    x1, x2     numbers that you want to multiply
          *@return   the result of the multiplication in double
          */
-        public static double Mul(double input1, double input2)
+        public static double Mul(double x1, double x2)
         {
-            return input1 * input2;
+            return x1 * x2;
         }
 
         /*
          * divides 2 double numbers
          *
-         *@param    input1,input2     numbers that you want to divide
+         *@param    x1, x2     numbers that you want to divide
          *@return   the result of the multiplication in double
          *          exception when dividing by 0
          */
-        public static double Div(double input1, double input2)
+        public static double Div(double x1, double x2)
         {
             //dividing by 0 check
-            if (input2 == 0)
+            if (x2 == 0)
                 throw new DivideByZeroException();
 
-            return input1 / input2;
+            return x1 / x2;
         }
 
         /*
          * calculates factorial of double number
          *
-         *@param    input1     the number to calculate the factorial from
+         *@param    x     the number to calculate the factorial from
          *@return   the factorial in double
          *          exception when the number is less than 0
          *          exception when the number is decimal
          */
-        public static double Fact(double input1)
+        public static double Fact(double x)
         {
             //negative number check
-            if (input1 < 0)
+            if (x < 0)
                 throw new ArgumentOutOfRangeException();
 
             //decimal number check
-            if ((input1 - Math.Truncate(input1)) != 0)
+            if ((x - Math.Truncate(x)) != 0)
                 throw new ArgumentOutOfRangeException();
 
             //factorial of zero is 1
-            if (input1 == 0)
+            if (x == 0)
                 return 1;
 
             //calculation of the factorial
-            double result = input1;
-            for (int i = 1; i < input1-1; i++)
+            double result = x;
+            for (int i = 1; i < x-1; i++)
             {
-                result *= (input1 - i);
+                result *= (x - i);
             }
             
             return result;
@@ -90,36 +90,36 @@ namespace MyMathLib
         /*
          * calculates natural power of real number
          *
-         *@param    input1      base number
-         *@param    input2      exponent
-         *@return   input1 raised to the input2
+         *@param    x      base number
+         *@param    n      exponent
+         *@return   x raised to the n
          *          exception when the exponent is less than 0
          *          exception when the base number and exponent are 0
          *          exeption when the exponent is decimal number
          */
-        public static double Pow(double input1, double input2)
+        public static double Pow(double x, double n)
         {
             //negative power check
-            if (input2 < 0)
+            if (n < 0)
                 throw new ArgumentOutOfRangeException();
 
             //zero power of the zero check
-            if ((input1 == 0) && (input2 == 0))
+            if ((x == 0) && (n == 0))
                 throw new InvalidOperationException();
 
-            //decimal number check
-            if ((input2 - Math.Truncate(input2)) != 0)
+            //decimal power check
+            if ((n - Math.Truncate(n)) != 0)
                 throw new ArgumentOutOfRangeException();
 
             //zero power of any number is always equal to 1
-            if (input2 == 0)
+            if (n == 0)
                 return 1;
 
             //calculation of the power
-            double result = input1;
-            for (int i = 1; i < input2; i++)
+            double result = x;
+            for (int i = 1; i < n; i++)
             {
-                result *= input1;
+                result *= x;
             }
 
             return result;
@@ -128,28 +128,28 @@ namespace MyMathLib
         /*
          * calculates n-th root of the real number
          *
-         *@param    input1      base number
-         *@param    input2      root
-         *@return   the root in input2 of the number in input1
+         *@param    x      base number
+         *@param    n      root
+         *@return   the x-th root of the number n
          *          exception when the root is 0
          *          exception when the root is even and base number is a negative number
          */
-        public static double Root(double input1, double input2)
+        public static double Root(double x, double n)
         {
             //zero base number check
-            if ((input2 == 0) && (input1 != 0))
+            if ((n == 0) && (x != 0))
                 throw new DivideByZeroException();
 
             //even base number and negative number check
-            if (((input2 % 2) == 0) && input1 < 0) 
+            if (((n % 2) == 0) && x < 0) 
                 throw new InvalidOperationException();
             
             //zero root of zero is equal to zero
-            if ((input1 == 0) && (input2 == 0))
+            if ((x == 0) && (n == 0))
                 return 0;
 
             //calculation of the root
-            return (double)Math.Pow(input1, 1.0 / input2);
+            return (double)Math.Pow(x, 1.0 / n);
         }
 
         /*
